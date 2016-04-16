14e1859166571bbfc736e49425915b2d7b6f6729
Homeros Bilişim <homerosbilisim@Homeros-Mac-mini.local>
2016-04-16 17:08:18 +0300

    first commit

---
+++ InstantImageDownloader/MyImagesCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyImagesCell.swift
+//  InstantImageDownloader
+//
+//  Created by Homeros Bilişim on 16.04.2016.
+//  Copyright © 2016 Homeros Bilişim. All rights reserved.
+//
+
+import Cocoa
+
+class MyImagesCell: UITableViewCell {
+
+}
